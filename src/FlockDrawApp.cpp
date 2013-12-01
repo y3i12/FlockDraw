32aa390b39aa71bd3f043e89b3bb89b1b851323d
Yuri Ivatchkovitch <mytos.mail@gmail.com>
2013-12-01 09:44:46 -0200

    Fix on command line image loading; Fix on image position on slideshow swap; Removed first demo image;

--- src/FlockDrawApp.cpp
+++ src/FlockDrawApp.cpp
@@ -13,19 +13,20 @@ using namespace ci::app;
 using namespace std;
 
 
-class HodginTourApp : public AppNative 
+class CinderApp : public AppNative 
 {
-  public:
+public:
 	void setup();
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
-  void prepareSettings( Settings *settings );
-  void fileDrop (ci::app::FileDropEvent e);
+	void prepareSettings( Settings *settings );
+	void fileDrop (ci::app::FileDropEvent e);
+  void updateOutputArea( Vec2i& _imageSize );
 
   Surface                     m_surface;
   gl::Texture                 m_texture;
-  Area                        outputArea;
+  Area                        m_outputArea;
   ParticleEmitter             m_particleEmitter;
   double                      m_lastTime;
   gl::Fbo                     m_frameBufferObject;
@@ -36,54 +37,42 @@ private:
   double                      m_cycleCounter;
 };
 
-void HodginTourApp::fileDrop (ci::app::FileDropEvent e)
+void CinderApp::fileDrop (ci::app::FileDropEvent e)
 {
   m_files        = e.getFiles();
   m_cycleCounter = m_cycleImageEvery;
 }
 
-void HodginTourApp::setup()
+void CinderApp::setup()
 {
+  m_cycleImageEvery = 15.0;
+
   try
   {
-   // fs::path appPath = getAppPath() + fs::path( "/assets" );
     if ( getArgs().size() > 1 )
     {
       const std::vector< std::string >& args = getArgs();
 
       for ( size_t i = 1; i < args.size(); ++i )
       {
-        m_files.push_back( fs::path( args[ i ] ) );
+        m_files.push_back( fs::canonical( fs::path( args[ i ] ) ) );
       }
 
-      m_cycleCounter = 0.0;
+      m_cycleCounter = m_cycleImageEvery;
     }
     else
     {
-      m_surface       = loadImage( loadAsset( "DSC_8489.jpg" ) );
       m_cycleCounter  = -1.0;
     }
 
-    m_texture = m_surface;
 
-    Vec2i imageSize = m_surface.getSize();
-  
-    outputArea.x1 = outputArea.x2 = static_cast< int >( getWindowCenter().x );
-    outputArea.y1 = outputArea.y2 = static_cast< int >( getWindowCenter().y );
-    
-    outputArea.x1 -= imageSize.x / 2;
-    outputArea.x2 += imageSize.x / 2;
-    outputArea.y1 -= imageSize.y / 2;
-    outputArea.y2 += imageSize.y / 2;
   }
   catch ( ... )
   {
     console() << "Unable to load the image" << std::endl;
   }
   
-  gl::Fbo::Format hdrFormat;
-  hdrFormat.setColorInternalFormat( GL_RGBA32F );
-  m_frameBufferObject = gl::Fbo( 800, 600, hdrFormat );
+  m_frameBufferObject = gl::Fbo( 800, 600, true );
   m_frameBufferObject.bindFramebuffer();
   gl::enableAlphaBlending();
   gl::clear( Color( 0.0f, 0.0f, 0.0f ) ); 
@@ -93,29 +82,40 @@ void HodginTourApp::setup()
   m_particleEmitter.m_minLifeTime        = 10.0;
   m_particleEmitter.m_fadeInTime         = 0.5f;
   m_particleEmitter.m_fadeOutTime        = 3.0f;
-  m_particleEmitter.m_position           = Vec2f( static_cast< float >( outputArea.x1 ), static_cast< float >( outputArea.y1 ) );
   m_particleEmitter.m_referenceSurface   = &m_surface;
   m_particleEmitter.m_screenTexture      = &m_frameBufferObject.getTexture();
   m_particleEmitter.m_particlesPerSecond = 0;
   
-  m_particleEmitter.addParticles( 300 );
-#if !defined _DEBUG
-  m_particleEmitter.addParticles( 300 );
-  m_particleEmitter.addParticles( 300 );
-  m_particleEmitter.addParticles( 300 );
-  m_particleEmitter.addParticles( 300 );
-#endif
-
   m_lastTime        = ci::app::getElapsedSeconds();
-  m_cycleImageEvery = 15.0;
 }
 
-void HodginTourApp::mouseDown( MouseEvent event )
+
+void CinderApp::mouseDown( MouseEvent event )
 {
+  m_cycleCounter = m_cycleImageEvery;
 }
 
-void HodginTourApp::update()
+
+void CinderApp::updateOutputArea( Vec2i& _imageSize )
 {
+    m_outputArea.x1 = m_outputArea.x2 = static_cast< int >( getWindowCenter().x );
+    m_outputArea.y1 = m_outputArea.y2 = static_cast< int >( getWindowCenter().y );
+    
+    m_outputArea.x1 -= _imageSize.x / 2;
+    m_outputArea.x2 += _imageSize.x / 2;
+    m_outputArea.y1 -= _imageSize.y / 2;
+    m_outputArea.y2 += _imageSize.y / 2;
+
+    m_particleEmitter.m_position = Vec2f( static_cast< float >( m_outputArea.x1 ), static_cast< float >( m_outputArea.y1 ) );
+}
+
+void CinderApp::update()
+{
+  if ( m_cycleCounter == -1.0 )
+  {
+    return;
+  }
+
   double currentTime = ci::app::getElapsedSeconds();
   double delta       = currentTime - m_lastTime ;
 
@@ -133,6 +133,8 @@ void HodginTourApp::update()
 
       m_surface = loadImage( aPath );
       m_texture = m_surface;
+
+      updateOutputArea( m_surface.getSize() );
 
       m_particleEmitter.killAll( currentTime );
 
@@ -152,8 +154,13 @@ void HodginTourApp::update()
   //m_particleEmitter.addParticles( 50 );
 }
 
-void HodginTourApp::draw()
+void CinderApp::draw()
 {
+  if ( m_cycleCounter == -1.0 )
+  {
+    return;
+  }
+
 	// clear out the window with black
 	gl::clear( Color( 0.0f, 0.0f, 0.0f ) ); 
 
@@ -167,10 +174,10 @@ void HodginTourApp::draw()
   m_frameBufferObject.blitFromScreen( getWindowBounds(), getWindowBounds() );
 }
 
-void HodginTourApp::prepareSettings( Settings *settings )
+void CinderApp::prepareSettings( Settings *settings )
 {
   settings->setWindowSize( 800, 600 );
   settings->setFrameRate( 60.0f );
 }
 
-CINDER_APP_NATIVE( HodginTourApp, RendererGl )
+CINDER_APP_NATIVE( CinderApp, RendererGl )
